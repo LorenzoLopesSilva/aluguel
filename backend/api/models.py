ac859d24aadb5c66fe9ee531aa4268df85e53c74
Lindomar <lindomarbatistao@gmail.com>
2026-02-05 11:25:13 -0300

    Back e Front

--- backend/api/models.py
+++ backend/api/models.py
@@ -30,4 +30,19 @@ class Imovel(models.Model):
 class Contrato(models.Model):
     data_inicio = models.DateField()
     data_fim = models.DateField(null=True, blank=True)
-    valor = models.
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    imovel = models.ForeignKey(Imovel, on_delete=models.CASCADE, related_name='contrato')
+    locador = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='locador')
+    locatario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='locatario')
+
+    def __str__(self):
+        return f"Contrato {self.id}"
+
+class Pagamento(models.Model):
+    data_pagamento = models.DateField()
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.BooleanField(default=False)
+    contrato = models.ForeignKey(Contrato, on_delete=models.CASCADE, related_name='pagamentos')
+
+    def __str__(self):
+        return f"Pagamento {self.id}"
---
+++ backend/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('usuarios', listar_usuarios),
+    path('users', UsuarioView.as_view()),
+    path('usuario/<int:pk>', UsuarioDetailView.as_view()),
+
+    path('imoveis', ImovelView.as_view()),
+    path('imovel/<int:pk>', ImovelDetailView.as_view()),
+
+    path('contratos', ContratoView.as_view()),
+    path('contrato/<int:pk>', ContratoDetailView.as_view()),
+
+    path('pagamentos', PagamentoView.as_view()),
+    path('pagamento/<int:pk>', PagamentoDetailView.as_view()),
+
+]
