828a029acedb6de70488b6eb707eb6124bf758da
LorenzoLopesSilva <lorenzo.lopes0808@gmail.com>
2026-02-20 11:28:31 -0300

    up

--- frontend/src/pages/home_user/index.jsx
+++ frontend/src/pages/home_user/index.jsx
@@ -1,13 +1,57 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 
-export default function HomeUser(){
+export default function HomeUser() {
     const token = localStorage.getItem('token')
+    const [usuarios, setUsuarios] = useState([])
 
-    return(
+    const getUsuarios = async () => {
+        try {
+            const response = await axios.get(
+                'http://127.0.0.1:8000/api/usuarios'
+
+            )
+
+            console.log("Deu certo");
+            setUsuarios(response.data)
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+    useEffect(() => {
+        getUsuarios()
+    }, [])
+
+    useEffect(() => {
+        console.log(usuarios[0])
+    }, [usuarios])
+
+
+
+    return (
         <div>
             <p>Essa é a página Home!!!</p>
-            <p>Token: {token}</p>
+            <table border={1}>
+                <tr>
+                    <th>Nome: </th>
+                    <th>Email: </th>
+                </tr>
+
+                {usuarios.map((usuario, index) => (
+                    <tr key={index}>
+                        <td>{usuario.nome}</td>
+                        <td>{usuario.email}</td>
+
+                    </tr>
+                ))}
+
+
+            </table>
+
         </div>
     )
 }
